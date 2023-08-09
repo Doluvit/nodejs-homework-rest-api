58c087cafc641a74126e5fbe7c9cdbe2331f140c
Doluvit <doluvit@gmail.com>
2023-08-09 13:26:59 +0300

    summary

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,96 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
+const router = express.Router();
+router.use(express.json());
 
-const router = express.Router()
+const contacts = require("../../models/index");
+const HttpError = require("../../helpers/HttpError");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .message(
+      "Invalid phone number format. Please use the format (XXX) XXX-XXXX."
+    ),
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      if (error.details[0].path[0] === "phone") {
+        throw HttpError(
+          400,
+          "Invalid phone number format. Please use the format (XXX) XXX-XXXX."
+        );
+      }
+      throw HttpError(400, "Missing required name field");
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      if (error.details[0].path[0] === "phone") {
+        throw HttpError(
+          400,
+          "Invalid phone number format. Please use the format (XXX) XXX-XXXX."
+        );
+      }
+      throw HttpError(400, "Missing required name field");
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
