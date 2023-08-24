a5872cb4c49fadd6c514759f4ce40c9bbbe61121
Doluvit <doluvit@gmail.com>
2023-08-24 23:17:39 +0300

    initial commit

---
+++ controllers/users/register.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcrypt");
+const { HttpError, ctrlWrapper } = require("../../helpers");
+const { User } = require("../../models/user");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashedPasword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashedPasword });
+
+  res
+    .status(201)
+    .json({
+      user: { email: newUser.email, subscription: newUser.subscription },
+    });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+};
