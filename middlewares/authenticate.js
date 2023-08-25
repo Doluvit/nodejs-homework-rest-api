ce2e32c576ef39f46b7aafcafdd96adeb15f1ce1
Doluvit <doluvit@gmail.com>
2023-08-25 13:04:17 +0300

    new authorizarion  check added

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,8 +7,13 @@ const {User} = require("../models/user")
 const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
+   
+    if (!authorization) {
+      return next(HttpError(401));
+    }
+   
     if (bearer !== "Bearer") {
-        next(HttpError(401))
+       return next(HttpError(401))
     }
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
